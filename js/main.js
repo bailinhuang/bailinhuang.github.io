70f1fbb13e395d71b9acda908b2fd8ac64f643df
Bailin Huang <Bailin Huang>
2019-01-15 23:30:38 -0600

    added content

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-      });
-  });
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});
+	});
 });
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// 	anchor.addEventListener('click', function (e) {
+// 		var e = document.getElementsByClassName("addAnimation");
+// 		Array.prototype.forEach.call(e, e => {
+// 			e.classList.add("animated");
+// 			e.classList.add("slideInUp");
+// 		});
+// 	});
+// });
+
+
+// function mas(e) {
+// 	var e = document.getElementsByClassName("addAnimation");
+// 	Array.prototype.forEach.call(e, e => {
+// 		e.classList.remove("animated");
+// 		e.classList.remove("slideInUp");
+// 	});
+
+// 	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// 		setTimeout(mas, 3000);
+// 		anchor.addEventListener('click', mas);
+
+// 	}
+// 	)};
