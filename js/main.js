2731cb36f0a474dce37f8b50f0ab79c7bd488d73
Bailin Huang <bailinhuangq@hotmail.com>
2020-01-13 22:48:08 +0100

    Added error page and removed unused code

--- js/main.js
+++ js/main.js
@@ -12,27 +12,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-// 	anchor.addEventListener('click', function (e) {
-// 		var e = document.getElementsByClassName("addAnimation");
-// 		Array.prototype.forEach.call(e, e => {
-// 			e.classList.add("animated");
-// 			e.classList.add("slideInUp");
-// 		});
-// 	});
-// });
-
-
-// function mas(e) {
-// 	var e = document.getElementsByClassName("addAnimation");
-// 	Array.prototype.forEach.call(e, e => {
-// 		e.classList.remove("animated");
-// 		e.classList.remove("slideInUp");
-// 	});
-
-// 	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-// 		setTimeout(mas, 3000);
-// 		anchor.addEventListener('click', mas);
-
-// 	}
-// 	)};
